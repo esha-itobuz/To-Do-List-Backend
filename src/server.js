6ffb22eb06f2e38571ea84d128ee516b772a082e
Esha <esha@itobuz.com>
2025-09-26 11:07:32 +0530

    feat: post request made

--- src/server.js
+++ src/server.js
@@ -1,17 +1,41 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
 const app = express()
+app.use(express.json())
+
+const dbPath = path.join(__dirname, '..', 'database', 'db.json')
 const port = 3000
 
-// const db = require('../database/db.json')
+function readDB() {
+  const data = fs.readFileSync(dbPath)
+  return JSON.parse(data)
+}
 
-// let users = db;
+function writeDB(content) {
+  fs.writeFileSync(dbPath, JSON.stringify(content, null, 2))
+}
 
-// app.use(express.json())
+app.post('/', (req, res) => {
+  const { title, tags } = req.body
 
-// app.post('/api/data', (req, res) => {
-//     const newData = req.body
+  if (!title) {
+    return res.status(400).json({ error: 'Title is required' })
+  }
+  const db = readDB()
+  const newToDo = {
+    id: db.todos.length + 1,
+    title,
+    createdAt: new Date().toISOString(),
+    isCompleted: false,
+    tags: tags || [],
+  }
+  db.todos.push(newToDo)
+  writeDB(db)
 
-// })
+  res.status(201).json(newToDo)
+})
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: 'Server is up and running' })
