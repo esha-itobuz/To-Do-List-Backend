22e53c370ed1f24708845a54d0bbab0eac97a11f
Esha <esha@itobuz.com>
2025-10-10 18:39:19 +0530

    feat: search and sort functionality added

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -16,5 +16,7 @@ router.post('/', validationInstance.validateRequest, todoController.createTodo)
 router.put('/:id', validationInstance.updateRequest, todoController.updateTodo)
 router.patch('/:id', todoController.patchTodo)
 router.delete('/:id', todoController.deleteTodo)
+router.get('/search', todoController.searchTask)
+router.get('/sort', todoController.sortTask)
 
 export default router
